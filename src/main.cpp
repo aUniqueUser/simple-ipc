048a44eee79d25d2926e14d5826ffbec7fa29109
nullifiedcat <nullifiedcat@gmail.com>
2017-03-19 16:43:03 +0300

    cool display

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,15 @@ static bool is_server = false;
 struct server_data { int one; };
 struct user_data { bool test; };
 
+#define ESC_PRINTF(s, ...) printf("\033[" s, __VA_ARGS__)
+#define ESC_CUP(x, y) ESC_PRINTF("%d;%dH", (y), (x))
+#define ESC_EL(n) ESC_PRINTF("%dK", (n))
+#define ESC_SGR(n) ESC_PRINTF("%dm", (n))
+#define ESC_ED(n) ESC_PRINTF("%dJ", (n));
+
+#define TEXT_BOLD ESC_SGR(1)
+#define TEXT_NORMAL ESC_SGR(0)
+
 using peer_t = cat_ipc::Peer<server_data, user_data>;
 
 std::string server_name = "test_ipc_cat";
@@ -28,6 +37,43 @@ std::string server_name = "test_ipc_cat";
 peer_t& peer() {
 	static peer_t object(server_name, false, is_server);
 	return object;
+}
+
+void print_status() {
+	ESC_CUP(1, 1);
+	ESC_ED(2);
+	fflush(stdout);
+	ESC_CUP(2, 2);
+	TEXT_BOLD;
+	printf("ipc server %s", server_name.c_str());
+	ESC_CUP(3, 4);
+	printf("connected: ");
+	TEXT_NORMAL; printf("%d", peer().memory->peer_count); TEXT_BOLD;
+	ESC_CUP(3, 5);
+	printf("command count: ");
+	TEXT_NORMAL; printf("%lu", peer().memory->command_count); TEXT_BOLD;
+	ESC_CUP(3, 6);
+	printf("command memory pool stats: ");
+	CatMemoryPool::pool_info_s info;
+	peer().pool->statistics(info);
+	ESC_CUP(4, 8); ESC_EL(2);  printf("total:     ");
+	ESC_CUP(4, 9); ESC_EL(2);  printf("free:      ");
+	ESC_CUP(4, 10); ESC_EL(2); printf("allocated: ");
+	TEXT_NORMAL;
+	ESC_CUP(16, 8);  printf("%lu (%u blocks)", info.free + info.alloc, info.blkcnt);
+	ESC_CUP(16, 9); printf("%lu (%u blocks)", info.free, info.freeblk);
+	ESC_CUP(16, 10); printf("%lu (%u blocks)", info.alloc, info.allocblk);
+	ESC_CUP(3, 12);
+	TEXT_BOLD;
+	printf("peer list: ");
+	TEXT_NORMAL;
+	for (unsigned i = 0; i < peer().memory->peer_count; i++) {
+		if (!peer().memory->peer_data[i].free) {
+			printf("%u (%d) ", i, peer().memory->peer_data[i].pid);
+		}
+	}
+	ESC_CUP(1, 14);
+	fflush(stdout);
 }
 
 void process_message(cat_ipc::command_s& cmd, void* payload) {
@@ -65,24 +111,19 @@ int main(int argc, char** argv) {
 		while (true) {
 			peer().ProcessCommands();
 			peer().SweepDead();
-			printf("peer count: %u command count: %lu\n", peer().memory->peer_count, peer().memory->command_count);
-			for (unsigned i = 0; i < cat_ipc::max_peers; i++) {
-				if (!peer().memory->peer_data[i].free)
-					printf("%i: [%i] ", i, peer().memory->peer_data[i].pid);
-			}
-			printf("\n");
-			sleep(5);
+			print_status();
+			usleep(10000);
 		}
 	} else if (!strcmp(argv[1], "client")) {
 		peer().SetCallback(process_message);
 		pthread_t thread;
 		pthread_create(&thread, 0, listen_for_messages, 0);
-		char buffer[256];
+		char* buffer = new char[1024 * 1024 * 1024];
 		while (true) {
-			fgets(buffer, 256, stdin);
+			fgets(buffer, 1024 * 1024 * 1024, stdin);
 			buffer[strlen(buffer) - 1] = '\0';
 			if (strlen(buffer) > 63) {
-				peer().SendMessage(buffer, 0, buffer, 256);
+				peer().SendMessage(buffer, 0, buffer, strlen(buffer) + 1);
 			} else {
 				peer().SendMessage(buffer, 0, 0, 0);
 			}
